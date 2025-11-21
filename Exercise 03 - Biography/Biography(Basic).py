291d54a604077f2d432f9cf772ae98cfb0721e19
adwaithbinish-cell <adwaithbinish07@gmail.com>
2025-11-21 20:33:25 +0400

    code lab 1

---
+++ Exercise 03 - Biography/Biography(Basic).py
@@ -0,0 +1,10 @@
+## Exercise 3: Biography - 25 Marks
+
+#Storing the information in a dictionary
+Biography = {
+    "Name": "Adwaith",
+    "Hometown":"Ajman",
+    "Age":18
+}
+#Print the values
+print(f"Name: {Biography['Name']} \nHometown: {Biography['Hometown']} \nAge: {Biography['Age']}")
